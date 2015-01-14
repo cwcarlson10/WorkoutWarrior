ede85191ebbf8f1ff52d680bf87aa3dd82eea081
Kevin Murphy <km86463@gmail.com>
2015-01-14 16:23:13 -0700

    Changed the controller, then changed it back.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,13 +1,12 @@
 class ExercisesController < ApplicationController
 
 
-
   def index
     @exercise = Exercise.new
-    lower_body = ['Squats', 'Lunges', 'Calf Raises', 'Glute Kick-Back']
-    upper_body = ['Pull-ups', 'Bench Press', 'Rows', 'Push-ups']
-    core = ['Leg Lifts', 'Sit-Ups', 'Russian Twists', 'Plank']
-    cardio = ['Running', 'Elyptical', 'Jump Rope', 'Stair Climber']
+      @lower_body = ['Squats', 'Lunges', 'Calf Raises', 'Glute Kick-Back']
+      @upper_body = ['Pull-ups', 'Bench Press', 'Rows', 'Push-ups']
+      @core = ['Leg Lifts', 'Sit-Ups', 'Russian Twists', 'Plank']
+      @cardio = ['Running', 'Elyptical', 'Jump Rope', 'Stair Climber']
   end
 
 
