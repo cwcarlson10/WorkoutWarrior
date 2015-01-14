ebe80274dfd97a971b321e4fbffaff7179a95f70
Evan A <evjay.a@gmail.com>
2015-01-14 16:59:02 -0700

    updated the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,15 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-
-  get 'routines/new'
-
-  get 'routines/show'
-
-  get 'routines/index'
-
-  get 'routines/edit'
-
-
   root 'static_pages#home'
 
   get 'exercises' => 'exercises#index'
   get '/about', to: 'static_pages#about', as: :about
 
-
   resources :users, :only => [:show, :index]
   get '/profile', to: 'users#profile', as: :profile
 
   resources :programs
+  resources :routines
+
 end
