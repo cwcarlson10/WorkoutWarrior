e643bc9cb5809110790a0e9bde89f2077d0d3f1e
Chris Carlson <cwcarlson10@gmail.com>
2015-01-14 16:36:05 -0700

    Added views for user actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def profile
+    @user = current_user
+  end
+
+end
