6fcebba8fb696e760f7e683fa73a184f4862709f
Chris Carlson <cwcarlson10@gmail.com>
2015-01-15 15:56:54 -0700

    created trainer model and routes

---
+++ db/migrate/20150115224427_create_trainers.rb
@@ -0,0 +1,12 @@
+class CreateTrainers < ActiveRecord::Migration
+  def change
+    create_table :trainers do |t|
+      t.string :name
+      t.string :email
+      t.string :organization
+      t.string :certifications
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/trainers.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :trainer do
+    name "MyString"
+email "MyString"
+organization "MyString"
+certifications "MyString"
+  end
+
+end
