9583c16c6f567f18d5bdb876646f84c075298415
Nicholas Holley <nick@zcid.co>
2015-01-14 18:33:44 -0700

    Require user for program

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,8 +1,9 @@
 class Program < ActiveRecord::Base
+  belongs_to :user
+
   validates :name, format: { with: /\A[ a-zA-Z0-9]+[a-zA-Z0-9]+\z/,
                              message: "only allows letters" }
   validates :name, length: { maximum: 100 }
   validates :name, presence: true
-
-  belongs_to :user
+  validates :user_id, presence: true
 end
