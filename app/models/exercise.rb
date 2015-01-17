bb19d0a4ac6a51c5320711f9609dac2023e77739
Evan A <evjay.a@gmail.com>
2015-01-16 17:04:03 -0700

    cleaned up and added a class method for exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,11 +1,11 @@
 class Exercise < ActiveRecord::Base
   has_many :routines
-
-  # attr_accessor: :categories
-  #
-  # def categories
-  #   @categories = ['']
-  # end
-  has_many :routines
   has_many :programs, through: :routines
+
+  @categories = ["Upper Body", "Lower Body", "Cardio", "Core"]
+
+  def self.exercise_categories
+    return @categories
+  end
+
 end
