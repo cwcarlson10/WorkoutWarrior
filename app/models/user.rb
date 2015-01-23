e294bf0a4157aaedc561856a8b8a0cacf9bd8c38
evja <evjay.a@gmail.com>
2015-01-23 16:54:45 -0700

    Revert "Evan/program form search function"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,27 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_one :trainer
-  has_one :athlete
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  has_one :trainer, dependent: :destroy
+  has_one :athlete, dependent: :destroy
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [:facebook]
+
 
   validate :role, presence: true
 
   enum role: [:newuser, :athlete, :trainer]
 
   after_find :load_role
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth["info"]["email"]
+      user.password = Devise.friendly_token[0,20]
+      binding.pry
+      # user.username = auth.info.nickname I might need this later
+    end
+  end
 
   def trainer?
     !!self.trainer
