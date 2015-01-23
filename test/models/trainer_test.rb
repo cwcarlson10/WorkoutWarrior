eaf9e681518f24df3cfab09c3a2e087997dbc9c7
Nicholas Holley <nick@zcid.co>
2015-01-23 14:36:06 -0700

    Added Trainer model test stub

--- test/models/trainer_test.rb
+++ test/models/trainer_test.rb
@@ -3,24 +3,23 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @athlete_user = users(:athlete)
-    @athlete = Athlete.create(name: "Test User", user_id: @athlete_user.id)
     @trainer_user = users(:trainer)
-    @bad_athlete = Athlete.create(name: "Bad Test User", user_id: @trainer_user.id)
+
   end
 
-  test "athlete should have a user_id" do
+  test "trainer should have a user_id" do
     assert @athlete.valid?
     @athlete.user_id = nil
     assert_not @athlete.valid?
   end
 
-  test "athlete should have a name" do
+  test "trainer should have a name" do
     assert @athlete.valid?
     @athlete.name = nil
     assert_not @athlete.valid?
   end
 
-  test "athlete's user should have a role of athlete" do
+  test "trainer's user should have a role of trainer" do
     assert_not @bad_athlete.valid?
     assert @athlete.valid?
   end
