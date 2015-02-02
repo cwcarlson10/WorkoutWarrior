a3b4605cf5b279ed60548b6e09835536b641c76a
Nicholas Holley <nick@zcid.co>
2015-02-02 15:15:56 -0700

    Update RoleValidator and add to Trainer model

--- app/validators/role_validator.rb
+++ app/validators/role_validator.rb
@@ -1,7 +1,7 @@
 class RoleValidator < ActiveModel::Validator
   def validate(record)
-    if User.find_by_id(record.user_id).try(:role) != options[:role]
-      record.errors[:base] << "User role is not properly assigned."
+    unless User.find_by_id(record.user_id).try(:role) == options[:role]
+      record.errors[:base] << "User role: #{options[:role]} is not properly assigned to user: #{record.user_id}."
     end
   end
 end
