70c91d445543cce7e83fd35f351477c10c180aa1
Chris Carlson <cwcarlson10@gmail.com>
2015-02-03 15:18:00 -0700

    100% coverage on athlete_controller

--- test/controllers/athletes_controller_test.rb
+++ test/controllers/athletes_controller_test.rb
@@ -4,23 +4,21 @@ class AthletesControllerTest < ActionController::TestCase
 include Devise::TestHelpers
 
   setup do
-    @athlete = athletes(:athlete)
+    sign_in User.first
+    @athlete = athletes(:athlete_1)
   end
 
   test "should get index" do
-    sign_in User.first
     get :index
     assert_response :success
   end
 
   test "should get show" do
-    sign_in User.first
-    get :show, id: User.first.id
+    get :show, id: @athlete.id
     assert_response :success
   end
 
   test "should get new" do
-    sign_in User.first
     get :new
     assert_response :success
   end
@@ -29,11 +27,36 @@ include Devise::TestHelpers
     assert_difference('Athlete.count') do
       post :create, athlete: @athlete.attributes
     end
+
+    assert_redirected_to athlete_path(assigns(:athlete))
+  end
+
+  test "should render new unless successful create" do
+    post :create, athlete: {name: nil}
+    assert_template 'new'
   end
 
   test "should get edit" do
-    sign_in User.first
-    get :edit, id: User.first.id
+    get :edit, id: @athlete.id
     assert_response :success
+  end
+
+  test "should update athlete" do
+    patch :update, {id: @athlete.id, athlete: {trainer_id: @athlete.trainer_id, name: "Jimbo Connor"}}
+
+    assert_redirected_to athlete_path(assigns(:athlete))
+  end
+
+  test "should render edit unless successful update" do
+    patch :update, {id: @athlete.id, athlete: {trainer_id: @athlete.trainer_id, name: nil}}
+    assert_template 'edit'
+  end
+
+  test "should delete athlete" do
+    assert_difference('Athlete.count', - 1) do
+      delete :destroy, id: @athlete
+    end
+
+    assert_redirected_to root_path
   end
 end
