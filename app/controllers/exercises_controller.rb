a6b29f23931bbe247ef64c149b190c5460c08498
Nicholas Holley <nick@zcid.co>
2015-02-03 14:36:23 -0700

    Remove api method in exercises controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -7,20 +7,3 @@ class ExercisesController < ApplicationController
     	@core = Exercise.where(category: 'Core')
     	@cardio = Exercise.where(category: 'Cardio')
   end
-
-  private
-
-  def get_exercises
-    page = 1
-    result = ''
-      while page <= 8
-        result_hash = HTTParty.get("https://wger.de/api/v2/exercise/?page=#{page}&language=2")
-        result_hash['next']
-        result_hash['results'].each do |result|
-          Exercise.create!(title: result['name'], description: result['description'])
-        end
-        page += 1
-      end
-  end
-
-end
