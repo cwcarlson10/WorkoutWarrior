9e289e4b1421182bf826ba7026f7f6d189354cb5
Nicholas Holley <nick@zcid.co>
2015-02-03 15:56:31 -0700

    Add static_pages_contoller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,5 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
 
+  def setup
+  end
+
+  test "Get / should return the home page" do
+    get :home
+    assert_response :success
+    assert_template :home
+  end
+
+  test "Get /about should return the home page" do
+    get :about
+    assert_response :success
+    assert_template :about
+  end
+
+  test "Get /role should return the role page" do
+    get :role
+    assert_response :success
+    assert_template :role
+  end
 end
