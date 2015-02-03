6d0eb2598a015a155615c5523a1f90a803fb661e
Chris Carlson <cwcarlson10@gmail.com>
2015-02-03 16:52:09 -0700

    added test to activites controller

--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class ActivitiesControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
-    assert_response :success
+include Devise::TestHelpers
+  setup do
+    @user = users(:athlete_user_1)
+    sign_in @user
+    @athlete = @user.athlete
+    @program = programs(:program_1)
+    @activity = Activity.create!(athlete_id: @athlete.id, program_id: @program.id)
   end
 
-  test "should get update" do
-    get :update
-    assert_response :success
+  test "should create activity" do
+    assert_difference('Activity.count') do
+      post :create, activity: {athlete_id: @athlete.id, program_id: @program.id}
+    end
+
+    assert_redirected_to athlete_path(assigns(:athlete))
   end
 
 end
