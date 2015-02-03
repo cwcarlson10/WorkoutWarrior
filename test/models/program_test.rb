79d2620bc3c52aac816b667b52a7f1be0b253530
Nicholas Holley <nick@zcid.co>
2015-02-03 14:23:54 -0700

    Activity test

--- test/models/program_test.rb
+++ test/models/program_test.rb
@@ -3,16 +3,22 @@ require 'test_helper'
 class ProgramTest < ActiveSupport::TestCase
   def setup
     @program = programs(:program_1)
+    @trainer = @program.trainer
+    @name = @program.name
   end
 
   test "Program should have a name" do
     @program.name = nil
     assert_not @program.valid?
-    @program.name = "Program Name"
+    @program.name = @name
     assert @program.valid?
   end
 
   test "Program should have a trainer" do
+    @program.trainer = nil
+    assert_not @program.valid?
+    @program.trainer = @trainer
+    assert @program.valid?
   end
 
   test "Program.routines should return an array" do
