bf674c1b53e3014a4315397051006af55ddd6fff
Chris Carlson <cwcarlson10@gmail.com>
2015-02-07 15:51:16 -0700

    added more styling to the trainer profile page as well as the athlete show

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206052334) do
+ActiveRecord::Schema.define(version: 20150206072944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20150206052334) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.text     "bio"
   end
 
   add_index "trainers", ["user_id"], name: "index_trainers_on_user_id", using: :btree
